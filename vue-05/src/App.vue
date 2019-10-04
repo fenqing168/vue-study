c32ec78d067c1bd4dd15468bea600af3ab7aab3d
wangding <wangding>
2019-10-04 23:29:36 +0800

    vue webpack

---
+++ vue-05/src/App.vue
@@ -0,0 +1,27 @@
+<template>
+    <div>
+      <img class="logo" src="./assets/logo.png" alt="logo"/>
+      <HelloWolrd />
+    </div>
+</template>
+
+<script>
+
+  import HelloWolrd from './components/HelloWorld'
+
+  export default {
+    name: 'App',
+    components: {
+      HelloWolrd
+    }
+  }
+</script>
+
+<style scoped>
+
+  .logo{
+    width: 200px;
+    height: 200px;
+  }
+
+</style>
---
+++ vue-05/src/components/HelloWorld.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <p class="msg">{{msg}}</p>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'HelloWorld',
+    data () {
+      return {
+        msg: 'Hello World Component'
+      }
+    }
+  }
+</script>
+
+<style scoped>
+  .msg {
+    color: red;
+    font-size: 30px;
+  }
+</style>
