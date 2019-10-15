30728d8e2e6fae3ef1308ccbb8cde89e89a03bad
wangding <wangding>
2019-10-16 00:27:46 +0800

    初次导入

--- vue-09/src/App.vue
+++ vue-09/src/App.vue
@@ -1,28 +1,50 @@
 <template>
-  <div id="app">
-    <img src="./assets/logo.png">
-    <HelloWorld/>
+  <div>
+    <input type="text" class="form-control" @keyup="getKeywords" placeholder="关键字" v-model="keyword">
+    <button @click="getKeywords">搜索</button>
+    <ul v-if="keywords.length != 0">
+      <li v-for="(keyword, index) in keywords" :key="index">{{keyword}}</li>
+    </ul>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld'
+import 'bootstrap/dist/css/bootstrap.css'
+import axios from 'axios'
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  data () {
+    return {
+      keyword: null,
+      keywords: []
+    }
+  },
+  mounted () {
+    // this.getKeywords()
+  },
+  methods: {
+    getKeywords () {
+      if (!this.keyword || this.keyword === '') {
+        this.keywords = []
+        console.log(this)
+        return
+      }
+      const arr = []
+      for (let i = 0; i < 10; i++) {
+        arr.push(
+          this.keyword + String.fromCharCode(
+            parseInt(
+              Math.random() * 100 + ''
+            )
+          )
+        )
+      }
+      this.keywords = arr
+    }
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>
